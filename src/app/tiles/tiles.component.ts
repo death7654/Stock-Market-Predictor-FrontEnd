11b9dd253308dc91eba0f3964ce7f98215fd9613
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-02-22 15:58:24 +0530

    add tiles

---
+++ src/app/tiles/tiles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tiles',
+  imports: [],
+  templateUrl: './tiles.component.html',
+  styleUrl: './tiles.component.scss'
+})
+export class TilesComponent {
+
+}
