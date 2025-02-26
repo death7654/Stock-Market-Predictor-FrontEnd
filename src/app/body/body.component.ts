e9915b61c011b679e3ba759bdee6783c1825eaef
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-02-26 19:42:32 +0530

    add stock markets

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -10,7 +10,86 @@ import { TilesComponent } from "./tiles/tiles.component";
 export class BodyComponent {
 
   data = [
-    
+    {
+      name: "GOOG",
+      image: "../../assets/icons/google.svg"
+    },
+    {
+      name: "AAPL",
+      image: ""
+    },
+    {
+      name: "MSFT",
+      image: ""
+    },
+    {
+      name: "META",
+      image: ""
+    },
+    {
+      name: "TSLA",
+      image: ""
+    },
+    {
+      name: "TWTR",
+      image: ""
+    },
+    {
+      name: "NVDA",
+      image: ""
+    },
+    {
+      name: "AMZN",
+      image: ""
+    },
+    {
+      name: "LLY",
+      image: ""
+    },
+    {
+      name: "ORCL",
+      image: ""
+    },
+    {
+      name: "COST",
+      image: ""
+    },
+    {
+      name: "NFLX",
+      image: ""
+    },
+    {
+      name: "JNJ",
+      image: ""
+    },
+    {
+      name: "HD",
+      image: ""
+    },
+    {
+      name: "BAC",
+      image: ""
+    },
+    {
+      name: "TMUS",
+      image: ""
+    },
+    {
+      name: "CSCO",
+      image: ""
+    },
+    {
+      name: "WTC",
+      image: ""
+    },
+    {
+      name: "TM",
+      image: ""
+    },
+    {
+      name: "IBM",
+      image: ""
+    }
   ]
 
 }
