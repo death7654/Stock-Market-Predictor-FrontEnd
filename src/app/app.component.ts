a630425b8bdfa407a02f2e1ca1a7eec55a4ecfe0
Abin Krishna <abinkrishna57@gmail.com>
2025-02-28 17:42:54 +0530

    Added function to the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
+// import { RouterOutlet } from '@angular/router';
+// import { NavbarComponent } from "./navbar/navbar.component";
 import { StartComponent } from "./start/start.component";
 import { BodyComponent } from "./body/body.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, StartComponent, BodyComponent],
+  imports: [StartComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { start } from 'repl';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  activeLink = 'home';
+
+  setActive(link: string) {
+    this.activeLink = link;
+  }
+
+  scrollToTop(){
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  }
+
+  scrollToPricing() {
+    const pricingSection = document.getElementById('pricing');
+    if (pricingSection) {
+      pricingSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+  scrollToDiscover() {
+    const discoverSection = document.getElementById('discover');
+    if (discoverSection) {
+      discoverSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  scrollToSupMarkets(){
+    const supmarkSection = document.getElementById('supmark');
+    if (supmarkSection) {
+      supmarkSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
