b10e3b2149d830108a076974a7eef1ce37585c9c
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-02-28 12:12:11 +0530

    float

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
